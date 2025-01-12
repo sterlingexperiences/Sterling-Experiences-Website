c8c70c8032619a48e9b3835fb727d9609c6c4cba
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-01-12 22:19:52 +0100

    about header mobile

--- src/components/about-components/Header.tsx
+++ src/components/about-components/Header.tsx
@@ -1,17 +1,17 @@
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-center items-end gap-[32px] bg-[#FFD700] rounded-[16px] h-[411px] pb-[18px] about_bg">
-        <h1 className="rounded-r-[16px] rounded-b-[16px] bg-[#800080] px-[24px] font-ebGaramond font-[600] text-[56px] text-[#FFD700] leading-[78px] tracking-[-0.04em] hover:bg-[#FFD700] hover:text-[#800080] transition-all duration-300">
+      <div className="flex flex-col md:flex-row justify-center items-end gap-[32px] bg-[#FFD700] rounded-[16px] h-[411px] pb-[18px] about_bg">
+        <h1 className="rounded-r-[16px] rounded-b-[16px] bg-[#800080] px-[24px] py-[20px] font-ebGaramond font-[600] text-[36px] md:text-[56px] text-[#FFD700] leading-[43px] md:leading-[78px] tracking-[-0.04em] hover:bg-[#FFD700] hover:text-[#800080] transition-all duration-300">
           We Create
         </h1>
-        <h1 className="font-ebGaramond rounded-l-[16px] rounded-b-[16px] px-[24px] font-[600] text-[56px] text-white bg-[#800080] leading-[78px] tracking-[-0.04em] hover:bg-white hover:text-[#800080] transition-all duration-300">
+        <h1 className="font-ebGaramond rounded-l-[16px] rounded-b-[16px] px-[24px] py-[20px] font-[600] text-[36px] md:text-[56px] text-white bg-[#800080] leading-[43px] md:leading-[78px] tracking-[-0.04em] hover:bg-white hover:text-[#800080] transition-all duration-300">
           Sterling Experiences
         </h1>
       </div>
 
-      <div className="space-y-[16px] px-[100px] my-[60px]">
-        <p className="font-openSans font-[400] text-[18px] leading-[28px] tracking-[-0.02em] text-[#0E0E12]">
+      <div className="space-y-[16px] md:px-[100px] my-[60px]">
+        <p className="font-openSans font-[400] text-[14px] md:text-[18px] leading-[16px] md:leading-[28px] tracking-[-0.02em] text-[#0E0E12]">
           Sterling Experiences is an event management company based in Nigeria,
           that specializes in creating exceptional and memorable experiences for
           businesses. A company founded out of the captivation of the magic of
@@ -20,7 +20,7 @@ const Header = () => {
           non-profits and experience firsthand the transformative power of
           well-planned events.
         </p>
-        <p className="font-openSans font-[400] text-[18px] leading-[28px] tracking-[-0.02em] text-[#0E0E12]">
+        <p className="font-openSans font-[400] text-[14px] md:text-[18px] leading-[16px] md:leading-[28px] tracking-[-0.02em] text-[#0E0E12]">
           Today, this passion has turned into a reality with Sterling
           Experiences. We specialize in crafting memorable corporate events that
           not only entertain but also achieve your business objectives. With
